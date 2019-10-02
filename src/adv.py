ec34ce6a38dd79495096ac530007b3fff13ad7ae
krikaz <pascal.bou@protonmail.com>
2019-10-02 17:00:45 +0200

    added items carry/drop

--- src/adv.py
+++ src/adv.py
@@ -1,7 +1,9 @@
 import sys
+import random
 
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -36,10 +38,6 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-# for k, v in room.items():
-#     if v == room['outside'].n_to:
-#         print(k)
-
 #
 # Main
 #
@@ -47,6 +45,31 @@ room['treasure'].s_to = room['narrow']
 # Make a new player object that is currently in the 'outside' room.
 
 player = Player('Player_One', 'outside')
+
+# Declare all the items
+
+item = {
+    'blade': Item('Blade', 'sharped blade'),
+    'sword': Item('Sword', 'shiny sword'),
+    'shield': Item('Shield', 'wooden shield'),
+    'hammer': Item('Hammer', 'rusty hammer'),
+    'staff': Item('Staff', 'magical staff of ice'),
+}
+
+# Place items in rooms randomly
+
+# all_rooms = []
+# all_items = []
+
+# for k, v in room.items():
+#     all_rooms.append(k)
+# for k, v in item.items():
+#     all_items.append(k)
+
+# for _ in range(5):
+#     r0 = random.randint(1, len(room))
+#     r1 = random.randint(1, len(item))
+#     room[all_rooms[r0]].items.append(all_items[r1])
 
 # Write a loop that:
 #
@@ -62,11 +85,17 @@ player = Player('Player_One', 'outside')
 while True:
     print(f'You are at {player.current_room}')
     print(room[player.current_room].description)
+    print(f'Items in this room : {room[player.current_room].items}')
     choices = ['n', 'e', 's', 'w']
+    actions = ['get', 'take', 'drop']
     x = input('move to ')
-    x_to = x+'_to'
+    x = x.split(' ')
+    # print(x)
 
-    if x in choices:
+    if x[0] == 'q':
+        sys.exit()
+    elif x[0] in choices:
+        x_to = x[0]+'_to'
         if not hasattr(room[player.current_room], x_to):
             print('nothing there')
         else:
@@ -74,10 +103,25 @@ while True:
             for k, v in room.items():
                 if v == getattr(room[player.current_room], x_to):
                     player.set_current_room(k)
-                    break                                            
-    elif x == 'q':
-        sys.exit()
+                    break
+    elif x[0] == 'i' or x[0] == 'inventory':
+        print(f'You carry {player.inventory}')
+    elif x[0] in actions:
+        if x[0] == 'get' or x[0] == 'take':
+            if x[1] in room[player.current_room].items:
+                room[player.current_room].items.remove(x[1])
+                player.inventory.append(x[1])
+                item[x[1]].on_take()
+            else:
+                print(f'There is no {x[1]} in this room')
+        elif x[0] == 'drop':
+            if x[1] in player.inventory:
+                player.inventory.remove(x[1])
+                room[player.current_room].items.append(x[1])
+                item[x[1]].on_drop()
+            else:
+                print(f'There is no {x[1]} in your inventory')            
     else:
-        print('type n, e, s or w')
+        print('type n, e, s or w to move or q to quit')
 
     print('\n')
--- src/player.py
+++ src/player.py
@@ -5,6 +5,7 @@ class Player():
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
             
     def set_current_room(self, current_room):
         self.current_room = current_room
